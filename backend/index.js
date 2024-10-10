a0e89c66e5e0db02285902894f70a252dfc086dc
Devanshu <design.devanshu@gmail.com>
2024-10-10 20:33:45 +0530

    refactor: update backend/index.js and package.json

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV || "production"; // Ensure the environment variable is set
 
-// Define the path to the frontend dist directory
+// Define the path to the frontend's dist directory
 const frontendDistPath = path.join(__dirname, "..", "frontend", "dist");
 
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -25,49 +26,36 @@ mongoose
     console.log("Database ERROR", err);
   });
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    // credentials: true,
-  }),
-);
-
-if (env === "DEVELOPMENT") {
-  app.use(require("./routes/gateway/webHook"));
-} else {
-  app.use("/api", require("./routes/gateway/webHook"));
-}
-
-// Cokkies Creation
+// Middleware
+const origins = process.env.ORIGINS.split(",");
+app.use(cors({ origin: origins })); // Adjust CORS for production
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
 app.use(cookieParser());
 
-if (env === "DEVELOPMENT") {
+// Define routes
+if (env === "development") {
+  app.use(require("./routes/gateway/webHook"));
   app.use(require("./auth"));
   app.use(require("./routes/index"));
 } else {
-  app.use(`/api`, require("./auth"));
-  app.use(`/api`, require("./routes/index"));
+  app.use("/api", require("./routes/gateway/webHook"));
+  app.use("/api", require("./auth"));
+  app.use("/api", require("./routes/index"));
 }
 
-app.use(require("./auth"));
-
+// Serve static files from the frontend's dist directory
 app.use(express.static(frontendDistPath));
 
 app.get("/healthz", (req, res) => {
   res.status(200).send("Server is running");
 });
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(frontendDistPath);
-});
-
+// Serve the React app's main HTML file for all other routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(frontendDistPath, "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Serving static files from: ${frontendDistPath}`);
   console.log(`Server is running on http://localhost:${port}`);
 });
